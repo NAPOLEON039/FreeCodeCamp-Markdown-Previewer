debb83ee357cf4602257347297531cb3460ce85f
NAPOLEON039 <nihar.raote@gmail.com>
2020-01-31 12:53:16 +0530

    Used a markdown library
    Installed markdown-it-vue and connected it to the contents of the textarea. It converts the markdown from the textarea in real time.

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,39 @@
 <template>
   <div id="app">
     <textarea v-model="inputText" name="text-editor" id="normal-text" cols="30" rows="10"></textarea>
-    <textarea v-model="markdownResult"></textarea>
+    <div id="markdown-result">
+      <markdown :content="inputText" />
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MarkdownItVue from 'markdown-it-vue'
 
 export default {
   name: 'app',
   data() {
     return {
-      inputText: '',
-      markdownResult: ''
+      inputText: ''
+    }
+  },
+  components: {
+    Markdown: MarkdownItVue
+  },
+  methods: {
+    convertToMarkdown() {
+      document.querySelector('#markdown-result').innerHTML = marked(this.inputText)
     }
   }
 }
 </script>
 
 <style>
-#app {
+#markdown-result {
+  display: inline-block;
+  margin: 0 10px;
+  border: 0.5px solid rgb(99, 97, 97);
+  height: 150px;
+  width: 300px;
 }
 </style>
