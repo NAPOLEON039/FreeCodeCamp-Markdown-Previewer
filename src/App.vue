a712f871de3cadb03826f957086a268c5f3d3cf1
NAPOLEON039 <nihar.raote@gmail.com>
2020-02-10 11:52:29 +0530

    Added some CSS
    Brought both the text editor and the markdown result boxes to the center of the page. Added word-wrap and scrolling to the markdown result. Changed the height and width for the text editor.

--- src/App.vue
+++ src/App.vue
@@ -19,21 +19,38 @@ export default {
   },
   components: {
     Markdown: MarkdownItVue
-  },
-  methods: {
-    convertToMarkdown() {
-      document.querySelector('#markdown-result').innerHTML = marked(this.inputText)
-    }
   }
 }
 </script>
 
-<style>
+<style scoped>
+#app {
+  position: fixed;
+  left: 0;
+  top: 0;
+  padding: 0;
+  margin: 0; 
+  height: 100%;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#normal-text {
+  height: 150px;
+  width: 300px;
+}
+
 #markdown-result {
   display: inline-block;
   margin: 0 10px;
   border: 0.5px solid rgb(99, 97, 97);
   height: 150px;
   width: 300px;
+  background-color: white;
+  white-space: pre-wrap;
+  word-wrap:break-word;
+  overflow-y: scroll;
 }
 </style>
